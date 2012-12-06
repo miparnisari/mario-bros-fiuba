8cb293aaa4eb0d9b799f9678a825e403b736eab9
maineparnisari <maineparnisari@dbae5173-42d3-1c62-9cf2-ddcd5ee66165>
2012-12-06 03:39:58 +0000

    Borro 2 clases inutiles.

--- src/vista/Espejo.h
+++
@@ -1,9 +0,0 @@
-#ifndef ESPEJO_H_
-#define ESPEJO_H_
-
-#include <SDL/SDL.h>
-
-SDL_Surface *MirrorSurfaceY( SDL_Surface *screen );
-SDL_Surface *MirrorSurfaceX( SDL_Surface *screen );
-
-#endif /* ESPEJO_H_ */
